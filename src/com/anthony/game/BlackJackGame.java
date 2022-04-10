ab9ce34a371b83ccb12433fb78158f7b482ec511
Anthony Yos <anthonyvyos@gmail.com>
2022-04-10 15:15:31 -0500

    Added constructor injection and setupGame()

--- src/com/anthony/game/BlackJackGame.java
+++ src/com/anthony/game/BlackJackGame.java
@@ -9,39 +9,42 @@ public class BlackJackGame {
     private final int blackjack;
     private final int minDealerHandValue;
     private boolean playGame;
+    private final Player thePlayer;
+    private final Dealer theDealer;
+    private final Deck theDeck;
 
-    public BlackJackGame() {
+    public BlackJackGame(Player thePlayer, Dealer theDealer, Deck theDeck) {
         blackjack = 21;
         minDealerHandValue = 17;
         playGame = true;
+        this.thePlayer = thePlayer;
+        this.theDealer = theDealer;
+        this.theDeck = theDeck;
     }
 
-    public void playBlackjack(String playerName) {
+    public void playBlackjack() {
 
         while (playGame) {
             // Create Deck, player, and dealer
-            Player thePlayer = new Player(playerName);
-            Dealer theDealer = new Dealer();
-            Deck playDeck = new Deck();
-            playDeck.shuffle();
+            setupGame();
 
             // Deal out cards and show current hands
             for (int i = 0; i < 2; i++) {
-                thePlayer.addCard(playDeck.deal());
-                theDealer.addCard(playDeck.deal());
+                thePlayer.addCard(theDeck.deal());
+                theDealer.addCard(theDeck.deal());
             }
             thePlayer.showHandInfo();
             theDealer.showSomeInfo();
 
             // Player asked to hit if possible/wanted
             if (thePlayer.getHandValue() < blackjack) {
-                GameAction.hitOrStand(thePlayer, playDeck);
+                GameAction.hitOrStand(thePlayer, theDeck);
                 thePlayer.showHandInfo();
             }
             // Full reveal dealer hand before possible hits
             theDealer.showHandInfo();
             while (theDealer.getHandValue() < minDealerHandValue)
-                GameAction.hit(theDealer, playDeck);
+                GameAction.hit(theDealer, theDeck);
 
             // Print final hand values
             System.out.println("Final Hands:");
@@ -52,6 +55,13 @@ public class BlackJackGame {
             GameEvaluator.evaluateGame(thePlayer, theDealer);
             playGame = GameAction.playAgain();
         }
+    }
+
+    private void setupGame(){
+        theDeck.resetDeck(thePlayer,theDealer);
+        thePlayer.resetPlayer();
+        theDealer.resetPlayer();
+        theDeck.shuffle();
     }
 
 }
